afd2dba19de94fb25a01b13b6eec6825b9fb5725
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-12-31 15:24:49 -0500

    Update cover.js

--- build/cover.js
+++ build/cover.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { relative } from './util.js';
+import {relative} from './util.js';
 
 const read = fs.readFileSync;
 const write = fs.writeFileSync;
@@ -11,8 +11,10 @@ const file = relative(import.meta, '..', 'docs', '_coverpage.md');
 let cover = read(file, 'utf8').toString();
 
 console.log('Replace version number in cover page...');
+
 cover = cover.replace(
   /<small>(\S+)?<\/small>/g,
   /* html */ `<small>${version}</small>`
 );
+
 write(file, cover);
